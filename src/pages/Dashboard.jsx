bb524914cd360c376b6498e7ca39ed09778abef3
Manan <manan.sharma2006gg@gmail.com>
2025-12-17 15:39:03 +0530

    Further

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,16 +1,27 @@
 import { FilePenLineIcon, PencilIcon, PlusIcon, TrashIcon, UploadCloudIcon, XIcon } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import { dummyResumeData } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
   const color = ["#9333ea", "#d97706", "#dc2626", "#0284c7", "#16a34a"];
 const [allResumes, setallResumes] = useState([]);       
 const [showCreatorResume, setshowCreatorResume] = useState(false);     
 const[showUploadResume,setShowUploadResume]  = useState(false);
-const[title ,settitle]  = useState(false);
+const[title ,settitle]  = useState('');
+const[resume ,setTitle]  = useState(null);
+const [editResumeId,seteditResumeId]=useState('')
+const navigate = useNavigate()
+
   const loadAllResumes = async () => {
     setallResumes(dummyResumeData);
   };
+  const createResume= async ()=>{
+    event.preventDefault()
+    setshowCreatorResume(false)
+    navigate(`/app/builder/res123`)
+
+  }
   useEffect(() => {
     loadAllResumes();
   }, []);
@@ -22,7 +33,7 @@ const[title ,settitle]  = useState(false);
         </p>
 
         <div className="flex gap-4">
-          <button className="w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group  hover:border-indigo-500 hover-shadow-lg transition-all duration-300 cursor-pointer">
+          <button onClick={()=>setshowCreatorResume(true)} className="w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group  hover:border-indigo-500 hover-shadow-lg transition-all duration-300 cursor-pointer">
             <PlusIcon className="size-11 transition-all duration-300 p-2.5 bg-gradient-to-br from indigo-300 to-indigo-500 text-white rounded-full " />
             <p className="text-sm group-hover:text-indigo-600 transition-all duration-300">
               Create Resume
@@ -71,14 +82,13 @@ const[title ,settitle]  = useState(false);
         </div>
         <div>
           {showCreatorResume && (
-            <form className="fixed inset-0 bg-black/70 backdrop:bllur bg-opacity-50 z-10 flex items-center justify-center">
-              <div>
-                <h2>Create a Resume</h2>
+            <form onSubmit={createResume} onClick={()=>setshowCreatorResume(false)}className="fixed inset-0 bg-black/70 backdrop:bllur bg-opacity-50 z-10 flex items-center justify-center">
+              <div onClick={e=>e.stopPropagation()
+              }className="relative bg-slate-50 border shadow-md rounded-lg w-full max-w-sm p-6">
+                <h2 className="text-xl font-bold mb-4">Create a Resume</h2>
                 <input type="text" placeholder="Enter resume title" className="w-full px-4 py-2 mb-4 focus:border-green-600 ring-green-600 ring-green-600" required/>
                 <button className="w-full py-2 bg-green-600 text-white rounded hover:bg-green-700 transition-colors">Create Resume</button>
-                <XIcon className="absolute top-4 right-4 text-slate-400 hover:text-slate-600 cursor-pointer transition-colors" onClick={()=>{setshowCreatorResume(false); 
-                  
-                }}/>
+                <XIcon className="absolute top-4 right-4 text-slate-400 hover:text-slate-600 cursor-pointer transition-colors" onClick={()=>{setshowCreatorResume(false); setTitle('')}}/>
               </div>
 
             </form>
