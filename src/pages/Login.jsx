2a25555b3501c4418727dad1ec29428bff762ea4
Manan <manan.sharma2006gg@gmail.com>
2025-12-17 14:33:41 +0530

    Further

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Login = () => {
   const query = new URLSearchParams(window.location.search);
   const urlState = query.get("state");
-  const [state, setState] = React.useState( urlState ||"login");
+  const [state, setState] = React.useState(urlState || "login");
 
   const [formData, setFormData] = React.useState({
     name: "",
@@ -67,14 +67,14 @@ const Login = () => {
             required
           />
         </div>
-        <div className="mt-4 text-left text-indigo-500">
+        <div className="mt-4 text-left text-green-500">
           <button className="text-sm" type="reset">
             Forget password?
           </button>
         </div>
         <button
           type="submit"
-          className="mt-2 w-full h-11 rounded-full text-white bg-indigo-500 hover:opacity-90 transition-opacity">
+          className="mt-2 w-full h-11 rounded-full text-white bg-green-500 hover:opacity-90 transition-opacity">
           {state === "login" ? "Login" : "Sign up"}
         </button>
         <p
@@ -85,7 +85,7 @@ const Login = () => {
           {state === "login"
             ? "Don't have an account?"
             : "Already have an account?"}{" "}
-          <a href="#" className="text-indigo-500 hover:underline">
+          <a href="#" className="text-green-500 hover:underline">
             click here
           </a>
         </p>
